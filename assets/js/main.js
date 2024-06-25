68adf56d4277367f60d656f308113dff4dfe9a9e
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2024-06-25 14:37:35 +0300

    WIP

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
- * Template Name: BergklangStudios
- * Template URL: https://bootstrapmade.com/BergklangStudios-bootstrap-website-template/
- * Updated: Jun 06 2024 with Bootstrap v5.3.3
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
- */
-
 (function () {
     'use strict';
 
